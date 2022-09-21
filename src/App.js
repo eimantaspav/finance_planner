470fc4297006e760883a6ee7b05569ed62f69f6f
eimantaspav <eimantaspav@gmail.com>
2022-09-21 15:27:31 +0300

    build routes, links (react-router-dom), add navbar + CSS

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Signup from './pages/signup/Signup';
 import Login from './pages/login/Login';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/signup" element={<Signup />}></Route>
