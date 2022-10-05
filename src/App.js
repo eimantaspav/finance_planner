bf7725e9c7bfdcdde86f9173349f5bb07f065597
eimantaspav <eimantaspav@gmail.com>
2022-10-05 15:31:28 +0300

    build auth state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import { Routes, Route } from 'react-router-dom';
+import { useAuthContext } from './hooks/useAuthContext';
+// pages
 import Home from './pages/home/Home';
 import Signup from './pages/signup/Signup';
 import Login from './pages/login/Login';
 import Navbar from './components/Navbar';
 
 function App() {
+  const { authIsReady } = useAuthContext();
   return (
     <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/signup" element={<Signup />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-      </Routes>
+      {authIsReady && (
+        <>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/signup" element={<Signup />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+          </Routes>
+        </>
+      )}
     </div>
   );
 }
